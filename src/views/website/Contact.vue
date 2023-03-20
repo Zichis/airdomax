ca83132c63ab0e6d1349b6234ff8a1546679adaf
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-03-20 08:43:59 +0100

    Fixed routes and pages for website.

---
+++ src/views/website/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an contact page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "ContactView",
+  components: {},
+};
+</script>
---
+++ src/views/website/Website.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "WebsiteView",
+};
+</script>
+
+<style lang="scss"></style>
