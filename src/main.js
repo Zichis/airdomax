f207b85cc48a1d4031106d43a83a14fffc98fb83
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-04-24 13:45:11 +0100

    Installed swiper.

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ import router from "./router";
 import store from "./store";
 import "aos/dist/aos.css";
 import "./assets/tailwind.css";
+import "swiper/css";
+import "swiper/css/autoplay";
 
 createApp(App).use(store).use(router).mount("#app");
