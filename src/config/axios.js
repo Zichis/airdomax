a7cb3723f215996e37d63dcc77987b53d369038a
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-05-13 13:20:57 +0100

    Installed axios.

---
+++ src/config/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const httpClient = axios.create({
+  baseURL: process.env.VUE_APP_BASE_URL,
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+  },
+});
