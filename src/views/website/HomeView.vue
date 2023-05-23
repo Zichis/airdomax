8c25b992e46baccb045544cb08568806a53e734b
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-05-23 02:28:24 +0100

    Changed button to routerlink.

--- src/views/website/HomeView.vue
+++ src/views/website/HomeView.vue
@@ -15,11 +15,12 @@
             <p>elit, sed do eiusmod tempor incididunt ut labore</p>
           </div>
           <div>
-            <button
+            <router-link
               class="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-7 py-3 mr-2 mb-2 dark:focus:ring-yellow-900"
+              :to="{ name: 'sign-up' }"
             >
               Sign Up
-            </button>
+            </router-link>
             <button
               class="py-3 px-7 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
